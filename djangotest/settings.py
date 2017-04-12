f352ec5f8303263e44398a69d3d444e34672cc32
Mark <mlang41110@yahoo.com>
2017-04-12 11:31:26 -0400

    added myapp

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -29,14 +29,21 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = [
+    'myapp',
 ]
 
 MIDDLEWARE = [
